bc196e7eb3f343e6f158fbec1571a64c6ef7b266
闫正伟 <yan934934@outlook.com>
2018-11-15 23:23:04 +0800

    2018/11/15 23:22 update

--- IntegerArraySort/C&C++/Sort.cpp
+++ IntegerArraySort/C&C++/Sort.cpp
@@ -22,3 +22,23 @@ bool bubblesort(int *arr, int length, int type) {
             }
     return true;
 }
+
+/**
+ * 插入排序
+ * @param arr       :   数组
+ * @param length    :   数组长度
+ * @param type      :   排序类型
+ *                      为0从小到大
+ *                      非0从大到小
+ */
+
+bool insertsort(int *arr,int length,int type){
+    if(length<2)
+        return false;
+    for(int i=1;i<length;i++){
+        int temp=arr[i],j;
+        for(j=i-1;j>=0&&type?arr[j]<temp:arr[j]>temp;j--)
+            arr[j+1]=arr[j];
+        arr[j+1]=temp;
+    }
+}
