2322f739222c0686157da5b4e99c76abd87a3b21
闫正伟 <yan934934@outlook.com>
2018-11-21 15:33:30 +0800

    2018/11/21 15:33 update

--- IntegerArraySort/C&C++/Sort.h
+++ IntegerArraySort/C&C++/Sort.h
@@ -1,5 +1,7 @@
 #ifndef SORT_SORT_H
 #define SORT_SORT_H
-bool bubblesort(int *arr,int length,int type);
-bool insertsort(int *arr,int length,int type);
+bool bubbleSort(int *arr,int length,int type);
+bool insertionSort(int *arr,int length,int type);
+bool selectionSort(int *arr,int length,int type);
+bool quickSort(int *arr,int length,int type);
 #endif //SORT_SORT_H
--- IntegerArraySort/C&C++/main.cpp
+++ IntegerArraySort/C&C++/main.cpp
@@ -2,18 +2,18 @@
 #include "Sort.h"
 
 int main(void){
-    int arr[]={1,2,4,5,6,4,7,8,5};
+    int arr[]={3,2,1,5,6,4,7,8,5,4,3,2,1,1,2,3,4,5};
     int length= sizeof(arr)/ sizeof(arr[0]);
 
-//    bubblesort(arr,length,0);
-//    bubblesort(arr,length,1);
-//    insertsort(arr,length,0);
-//    insertsort(arr,length,1);
-
+    bubbleSort(arr,length,0);
+//    bubbleSort(arr,length,1);
+//    insertSort(arr,length,0);
+//    insertSort(arr,length,1);
+//    selectionSort(arr,length,0);
+//    selectionSort(arr,length,1);
     for(int i=0;i<length;i++)
         printf("%d ",arr[i]);
 
-    return 0;
+return 0;
 
 }
-
